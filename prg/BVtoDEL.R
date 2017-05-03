2940bc15abd5247c454e764606880afe050356f2
neocarto <nicolas.lambert@ums-riate.fr>
2017-05-03 15:16:50 +0200

    BV

--- prg/BVtoDEL.R
+++ prg/BVtoDEL.R
@@ -8,10 +8,9 @@ load("data/geometriesTN.RData")
 
 # [2] Import de données correctement formatées
 
-BV.df<-read.csv( "data/Tableau Final_Avrl2017.csv",header=TRUE,sep=",",dec=".",encoding="utf-8")
+BV.df<-read.csv( "data/BV_v2.csv",header=TRUE,sep=",",dec=".",encoding="utf-8")
 
 # [3] Quelques resumés statistiques
-
 
 var <- BV.df$Total_T1
 hist(var, probability = TRUE, nclass = 30, col="lightgreen")
@@ -23,15 +22,23 @@ abline(v = med, col = "blue", lwd = 3)
 
 # [4] Aggregation des données par delegation
 colnames(BV.df)
+
+
+# Pour l'exercice, on ne selectionne que les BV sans erreurs
+
+BV.df <- BV.df[BV.df$erreur==0,]
+
 # choix des variables à sommer
-stocks <- colnames(BV.df)[15:length(BV.df)]
-stocks
-head(BV.df)
+
+stocks <- colnames(BV.df)[16:length(BV.df)-1]
+
+
 
 # Aggrégation
+
 VoteByDeleg.df <- aggregate(BV.df[,stocks], by = list(BV.df$del_id, BV.df$del_fr), FUN=sum)
 names(VoteByDeleg.df) <- c("id","name",stocks)
 
-
 # Export
 write.csv(VoteByDeleg.df,"data/VoteByDeleg.csv",row.names = F)
+
