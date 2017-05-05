4a4d9dcb22e717ccf25f987bad85d589058eb483
neocarto <nicolas.lambert@ums-riate.fr>
2017-05-05 10:01:43 +0200

    programme template pdf

--- prg/templatepdf.R
+++ prg/templatepdf.R
@@ -2,25 +2,34 @@
 # Création d'un template cartographique PDF  *
 # ********************************************
 
-#setwd("repertoire-de-travail")
+# Definition du répertoire de travail
 
-# [1] Chargement du fichier de données
+# setwd("repertoire-de-travail")
+
+# Chargement du fichier de données
 
 load("data/geometriesTN.RData")
 
-# [2] Mise en forme du template
+# appel du package 'cartography'
 
 library("cartography")
 
+# Définition de la taille de l'image à générer
 
+#sizes <- getFigDim(spdf = delegations.spdf, width = 15, mar = c(0, 0, 1.2, 0))
+k <- 0.393701 # cn to inches
+widthcm <- 15
+heightcom <- 20  
 
+# Ouverture d'une 'device' (sortie graphique) pdf
+
+pdf(file = "outputs/template.pdf", width = widthcm*k, height = heightcom*k,pointsize=15)
+
+# Choix des marges
 
 opar <- par(mar = c(0, 0, 1.2, 0), mfrow = c(1, 1))
-#sizes <- getFigDim(spdf = delegations.spdf, width = 15, mar = c(0, 0, 1.2, 0))
-k <- 0.5
-widthcm <- 15
-heightcom <- 18  
-pdf(file = "outputs/template.pdf", width = widthcm*k, height = heightcom*k,pointsize=15)
+
+# Construction du template
 
 plot(delegations.spdf, border = NA, col = NA, bg = "#A6CAE0")
 plot(countries.spdf,border="white",col="#eadac1", lwd=0.6, add=T)
@@ -34,9 +43,10 @@ layoutLayer(title = "Template cartographique de la Tunisie", author = "UMS RIATE
 barscale(size = 100, lwd = 1.5, cex = 0.6, pos = NULL,
          style = "pretty")
 
-
 #locator(1)
 text("ALGERIE",x=322630,y=3436484,cex = 1.5,col="#cca99280")
 text("LIBYE",x=800292.7,y=3477594,cex = 1.5,col="#cca99280")
+
+# On ferme la 'device' pdf
 
 dev.off()
